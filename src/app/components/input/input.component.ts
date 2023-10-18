432198967d5f36a7dd20071e0c1142cfc2288bfa
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-10-18 20:47:50 -0300

    feat: editando item

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -16,10 +16,14 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 export class InputComponent implements OnInit, OnChanges {
   @Input()
   itemQueVaiSerEditado!: Item;
+  editando = false;
+  textoBtn = 'Salvar item';
   valorItem!: string;
   constructor(private listaService: ListaDeCompraService) {}
   ngOnChanges(changes: SimpleChanges): void {
     if (!changes['itemQueVaiSerEditado'].firstChange) {
+      this.editando = true;
+      this.textoBtn = 'Editar item';
       this.valorItem = this.itemQueVaiSerEditado?.nome;
     }
   }
@@ -33,5 +37,15 @@ export class InputComponent implements OnInit, OnChanges {
 
   limparCampo() {
     this.valorItem = '';
+  }
+
+  editarItem() {
+    this.listaService.editarItemDaLista(
+      this.itemQueVaiSerEditado,
+      this.valorItem
+    );
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = 'Salvar item';
   }
 }
